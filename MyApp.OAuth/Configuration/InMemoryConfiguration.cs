bdfdb2357a37e0aff63b2fb4ea70ba7a63fe009d
Silvave <todor.vasilev@yahoo.com>
2018-06-14 12:49:50 +0300

    Adding IdentityServer4 + in memory configurations

---
+++ MyApp.OAuth/Configuration/InMemoryConfiguration.cs
@@ -0,0 +1,50 @@
+﻿namespace MyApp.OAuth.Configuration
+{
+    using IdentityServer4.Models;
+    using IdentityServer4.Test;
+    using System.Collections.Generic;
+
+    public class InMemoryConfiguration
+    {
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new[]
+            {
+                new ApiResource("myapp", "My App")
+            };
+        }
+
+        public static IEnumerable<Client> Clients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "myapp",
+                    ClientSecrets = new [] { new Secret("$3cr37".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new [] { "myapp" }
+                }
+            };
+        }
+
+        public static List<TestUser> Users()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "tester@testing.com",
+                    Password = "super_pass"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "my@account.com",
+                    Password = "super_pass"
+                }
+            };
+        }
+    }
+}
--- MyApp.OAuth/Startup.cs
+++ MyApp.OAuth/Startup.cs
@@ -4,6 +4,7 @@
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Http;
     using Microsoft.Extensions.DependencyInjection;
+    using MyApp.OAuth.Configuration;
 
     public class Startup
     {
@@ -11,15 +12,23 @@
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryClients(InMemoryConfiguration.Clients())
+                .AddInMemoryApiResources(InMemoryConfiguration.ApiResources())
+                .AddTestUsers(InMemoryConfiguration.Users());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
            if (env.IsDevelopment())
-           {
+            {
                 app.UseDeveloperExceptionPage();
-           }
+            }
+
+            app.UseIdentityServer();
 
             app.Run(async (context) =>
             {
