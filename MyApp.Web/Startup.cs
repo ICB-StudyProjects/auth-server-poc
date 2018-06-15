50a0131212c0a3570295311abbcb09a2a25e960a
Silvave <todor.vasilev@yahoo.com>
2018-06-15 15:49:21 +0300

    Added openid connect conf to .Web for connecting to Hybric Flow. Found why it didn't added the requested user's claims in the access_token

--- MyApp.Web/Startup.cs
+++ MyApp.Web/Startup.cs
@@ -36,9 +36,24 @@
                     options.Authority = "http://localhost:51364/";
                     options.RequireHttpsMetadata = false; // {true} in Production
 
+                    // When a client only requests response_type=id_token (which means no API is being used) then all the claims for the identity scopes requested go into the id_token. When the client requests response_type=id_token token, then (by default) only the sub claim goes into the id_token and the rest of the identity claims are returned via the userinfo endpoint.
                     // Added "token" to get "access token" back from the auth server
-                    options.ResponseType = "id_token token"; 
+                    options.ResponseType = "id_token"; // if "token" to get the "access_token" then only "sub" claims goes to the "id_token" 
                     options.ClientId = "myapp_implicit";
+
+                    // Authorization Code Flow Conf
+                    //options.ResponseType = "id_token code"; // Same goes for the claims as stated above
+                    //options.ClientId = "myapp_code";
+                    //options.ClientSecret = "$3cr37";
+                    //options.Scope.Add("myapp");
+                    //options.Scope.Add("offline_access");
+                    //options.GetClaimsFromUserInfoEndpoint = true;
+
+                    // Additional user scopes
+                    options.Scope.Add("email");
+                    options.Scope.Add("phone");
+                    options.Scope.Add("address");
+
                     options.SaveTokens = true;
                 });
         }
