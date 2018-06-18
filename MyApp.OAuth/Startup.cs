0a71be6acc7b9b1fa857d315aa6d1d2112b7d19c
Silvave <todor.vasilev@yahoo.com>
2018-06-18 11:59:49 +0300

    Adding store configurations for EF and db connection string

--- MyApp.OAuth/Startup.cs
+++ MyApp.OAuth/Startup.cs
@@ -5,22 +5,49 @@
     using Microsoft.Extensions.DependencyInjection;
     using MyApp.OAuth.Configuration;
     using System.IO;
+    using Microsoft.Extensions.Configuration;
     using System.Security.Cryptography.X509Certificates;
+    using Microsoft.EntityFrameworkCore;
 
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var authDbConnectionString = Configuration.GetConnectionString("MyApp.OAuth");
+            var assembly = typeof(Startup).GetType().Assembly.GetName().Name;
+
             services
                 .AddIdentityServer()
                 //.AddDeveloperSigningCredential()
                 .AddSigningCredential(new X509Certificate2(Directory.GetCurrentDirectory() + @"\Certificates\awesomenetwork.pfx", "test"))
                 .AddInMemoryIdentityResources(InMemoryConfiguration.IdentityResources())
-                .AddInMemoryApiResources(InMemoryConfiguration.ApiResources())
-                .AddInMemoryClients(InMemoryConfiguration.Clients())
-                .AddTestUsers(InMemoryConfiguration.Users());
+                // this adds the config data from DB (clients, resources)
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(authDbConnectionString,
+                            sql => sql.MigrationsAssembly(assembly));
+                })
+                // this adds the operational data from DB (codes, tokens, consents)
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(authDbConnectionString,
+                            sql => sql.MigrationsAssembly(assembly));
+
+                    // this enables automatic token cleanup. this is optional.
+                    //options.EnableTokenCleanup = true;
+                    //options.TokenCleanupInterval = 30;
+                });
 
             services.AddMvc();
         }
