c67305c2712a38d5d33742a4f399e1c09bde1bc9
Silvave <todor.vasilev@yahoo.com>
2018-06-18 17:33:27 +0300

    Adding Design-Time DbContext Creation Factory (for Migrations commands)

---
+++ MyApp.OAuth/Data/Migrations/IdentityServer/DesignTimeDbContextFactory.cs
@@ -0,0 +1,47 @@
+﻿namespace MyApp.OAuth.Data.Migrations.IdentityServer
+{
+    using IdentityServer4.EntityFramework.DbContexts;
+    using IdentityServer4.EntityFramework.Options;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+    using Microsoft.Extensions.Configuration;
+    using System.Reflection;
+
+    // HACK: Change to "PersistedGrantDbContext" or "ConfigurationDbContext", change the store and run the cli command
+    public sealed class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ConfigurationDbContext>
+    {
+        public ConfigurationDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .Build();
+
+            var builder = new DbContextOptionsBuilder<ConfigurationDbContext>();
+
+            var connStr = configuration.GetConnectionString("MyAppOAuthContext");
+            var assembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+            
+            builder.UseSqlServer(connStr, o => o.MigrationsAssembly(assembly));
+
+            return new ConfigurationDbContext(builder.Options, new ConfigurationStoreOptions());
+        }
+
+        //ConfigurationDbContext IDesignTimeDbContextFactory<ConfigurationDbContext>.CreateDbContext(string[] args)
+        //{
+        //    IConfigurationRoot configuration = new ConfigurationBuilder()
+        //    .AddJsonFile("appsettings.json")
+        //    .AddEnvironmentVariables()
+        //    .Build();
+
+        //    var builder = new DbContextOptionsBuilder<ConfigurationDbContext>();
+
+        //    var connStr = configuration.GetConnectionString("MyAppOAuthContext");
+        //    var assembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+        //    builder.UseSqlServer(connStr, o => o.MigrationsAssembly(assembly));
+
+        //    return new ConfigurationDbContext(builder.Options, new ConfigurationStoreOptions());
+        //}
+    }
+}
